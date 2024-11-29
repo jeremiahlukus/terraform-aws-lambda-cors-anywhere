13d6d1b8a76a9eeaebf89390b6d4854ff1b4f425
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2024-11-29 13:08:01 -0500

    rm allow orgin

--- aws_lambda_wrapper/index.js
+++ aws_lambda_wrapper/index.js
@@ -8,7 +8,6 @@ exports.handler = async function (event, context, callback) {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': '*',
         'Access-Control-Max-Age': '86400',
